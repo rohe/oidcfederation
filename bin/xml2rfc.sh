70a4d5bec726a950a90af4e0f096fbae218d88e3
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-10-09 13:21:58 +0200

    Remove scripts into travis ci instead

--- bin/xml2rfc.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-mkdir public
-docker run -v $(pwd):/rfc -w /rfc paulej/rfctools xml2rfc draft/oidcfed.hf.xml --text -o public/oidcfed-05.txt
-docker run -v $(pwd):/rfc -w /rfc paulej/rfctools xml2rfc draft/oidcfed.hf.xml --html -o public/oidcfed-05.html
-ls -la .
-ls -la public
-
-# mv oidcfed-05.html public/
