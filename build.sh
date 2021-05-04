ff9fcf7ea9b7b7b971dfce345ba18fb5673dc3f0
peppelinux <giuseppe.demarco@unical.it>
2021-05-04 09:28:15 +0200

    Build commands
    * feat: added build script
    * fix: list hangText style

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xml2rfc draft/openid-connect-federation-1_0.xml --v3 --text -o draft/openid-connect-federation-1_0.txt
+xml2rfc draft/openid-connect-federation-1_0.xml --v3 --html -o draft/openid-connect-federation-1_0.html
